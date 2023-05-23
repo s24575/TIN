b082da18580518915fd0422ef1c7ed11663c5185
s24575 <sawickigrzegorz66@gmail.com>
2023-05-23 14:39:27 +0200

    lab11

--- lab11/zad3/index.js
+++ lab11/zad3/index.js
@@ -1,16 +1,32 @@
-window.onload = function(){
-
+function showGallery(){
+    document.getElementById("myGallery").style.display = "block";
 }
 
-function openImage(n){
-    console.log(n);
-    const children = document.getElementsByClassName("column");
-    for(let i = 0; i < children.length; i++){
-        children[i].classList.add("hidden");
+function closeGallery(){
+    document.getElementById("myGallery").style.display = "none";
+}
+
+function nextSlide(n){
+    showSlides(index + n);
+}
+
+function selectSlide(n){
+    index = n;
+    showSlides(index);
+}
+
+let index = 1;
+
+function showSlides(n){
+    index = n;
+    let images = document.getElementsByClassName("image");
+    
+    if(index < 1) index = 1;
+    else if(index > images.length) index = images.length;
+
+    for(let i = 0; i < images.length; i++){
+        images[i].style.display = "none";
     }
-    showGallery(n);
-}
 
-function showGallery(n){
-    document.getElementsByClassName("image")[n - 1].classList.add("shown");
+    images[index - 1].style.display = "block";
 }
