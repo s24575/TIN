31e3716e7f5bbac6907e4e01bc47b7f7059e0a9a
s24575 <sawickigrzegorz66@gmail.com>
2023-05-30 17:29:04 +0200

    lab12

--- lab12/zad1/index.js
+++ lab12/zad1/index.js
@@ -3,22 +3,25 @@ $(document).ready(function () {
 
     function fetchData() {
         $.getJSON("http://szuflandia.pjwstk.edu.pl/~ppisarski/zad8/dane.php", function (data) {
-            if (JSON.stringify(data) !== JSON.stringify(lastData)) {
-                lastData = data;
-                updateDOM(data);
-            }
+            updateDOM(data);
+            lastData = data;
         });
     }
 
     function updateDOM(data) {
         var stockTable = $('#stock-table tbody');
-        stockTable.empty();
 
         $.each(data.stock, function (firma, kurs) {
-            var row = $('<tr>');
-            row.append($('<td>').text(firma));
-            row.append($('<td>').text(kurs));
-            stockTable.append(row);
+            var old = $(`tr:contains("${firma}")`);
+
+            if (old.length === 0 || old.text() !== firma + kurs) {
+                var row = $('<tr>');
+                row.append($('<td>').text(firma));
+                row.append($('<td>').text(kurs));
+                stockTable.append(row);
+            } else{
+                console.log("same");
+            }
         });
 
         updateNews(data.news);
@@ -27,28 +30,28 @@ $(document).ready(function () {
     function updateNews(data) {
         var newsRotator = $('#news-rotator');
         newsRotator.prepend($('<div class="banner">').text(data).fadeOut(0));
-        if(newsRotator.children().length > 3){
+        if (newsRotator.children().length > 3) {
             newsRotator.children().last().remove();
         }
     }
 
-    function rotate(){
+    function rotate() {
         var active = $("#news-rotator .banner.active");
         var next = active.next();
         if (next.length == 0)
-          next = $("#news-rotator .banner:first");
+            next = $("#news-rotator .banner:first");
         active.removeClass("active").fadeOut(200);
         next.addClass("active").fadeIn(200);
     }
 
-    function resetRotator(elem){
+    function resetRotator(elem) {
         $(elem + " .banner").fadeOut(0);
         $(elem + " .banner:first").fadeIn(0);
     }
-    
+
     setInterval(fetchData, 10000);
     fetchData();
-    
+
     resetRotator("#news-rotator");
     setInterval(rotate, 2500);
 });
